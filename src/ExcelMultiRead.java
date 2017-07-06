041a7f8f8da9204b32091c55c23e4433b6268c18
Smitha Shivakumar <shivakumar.smitha766@gmail.com>
2017-07-05 20:00:05 -0500

    removed duplicates entries with same zoneId and zoneName

--- src/ExcelMultiRead.java
+++ src/ExcelMultiRead.java
@@ -1,3 +1,9 @@
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -6,16 +12,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 
 public class ExcelMultiRead {
 
@@ -25,9 +29,9 @@ public class ExcelMultiRead {
 	 */
 	public static void main(String[] args) {
 
-		String FILE_NAME = "C:/Users/smitha/Downloads/PJM_V4.xlsx";
-		String targetDir = "C:/Users/smitha/Downloads";
-		String zipDir = "C:/Users/smitha/Downloads";
+		String FILE_NAME = "PJM_V4.xlsx";
+		String targetDir = "out";
+		String zipDir = "out";
 
 		File file = new File(FILE_NAME);
 		if (file.exists()) {
@@ -40,37 +44,49 @@ public class ExcelMultiRead {
 		InputStream inp = null;
 		byte[] buffer = new byte[1024];
 		try {
+			// print("inputFile.getName()" + inputFile.getName(), debugOn);
 			inp = new FileInputStream(inputFile);
+			// print("file read");
 			Workbook wb = WorkbookFactory.create(inp);
 			String filename = getFileName(inputFile);
-
-			FileOutputStream zipOut = new FileOutputStream(zipDir + "\\" + filename + ".zip");
-			System.out.println("zipOut==" + zipOut);
+			// print("targetDir=" + targetDir, debugOn);
+			// print("<wb.getNumberOfSheets()=" + wb.getNumberOfSheets(),
+			// debugOn);
+			FileOutputStream zipOut = new FileOutputStream(zipDir + File.separatorChar + filename + ".zip");
+			//System.out.println("zipOut==" + zipOut);
 			ZipOutputStream zos = new ZipOutputStream(zipOut);
-			for (int i = 0; i < wb.getNumberOfSheets(); i++) {
-				File fout = new File(targetDir + "\\" + filename + "_" + wb.getSheetAt(i).getSheetName() + ".csv");
-				System.out.println("fout=" + fout.getName());
-				FileOutputStream fos = new FileOutputStream(fout);
-				OutputStreamWriter osw = new OutputStreamWriter(fos);
+			String CSV_FILE = wb.getSheetAt(7).getSheetName();
+			System.out.println(CSV_FILE) ;
+			//for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+			// print(wb.getSheetAt(i).getSheetName(), debugOn);
+			//File fout = new File(targetDir + "\\" + filename + "_" + wb.getSheetAt(i).getSheetName() + ".csv");
+			File fout = new File(targetDir + File.separatorChar + "Zones.csv");
 
-				echoAsCSV(wb.getSheetAt(i), osw);
-				osw.close();
-				ZipEntry ze = new ZipEntry(fout.getName());
-				zos.putNextEntry(ze);
-				FileInputStream in = new FileInputStream(fout.getAbsolutePath());
+			//System.out.println("fout=" + fout.getName());
+			FileOutputStream fos = new FileOutputStream(fout);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
 
-				int len;
-				while ((len = in.read(buffer)) > 0) {
-					zos.write(buffer, 0, len);
-				}
+			//echoAsCSV(wb.getSheetAt(i), osw);
+			echoAsCSV(wb.getSheetAt(7), osw);
 
-				in.close();
-				zos.closeEntry();
+			osw.close();
+			ZipEntry ze = new ZipEntry(fout.getName());
+			zos.putNextEntry(ze);
+			FileInputStream in = new FileInputStream(fout.getAbsolutePath());
 
-				// remember close it
-
+			int len;
+			while ((len = in.read(buffer)) > 0) {
+				zos.write(buffer, 0, len);
 			}
+
+			in.close();
+			zos.closeEntry();
+
+			// remember close it
+
+			//}
 			zos.close();
+			// print("file closed");
 		} catch (InvalidFormatException ex) {
 			Logger.getLogger(ExcelMultiRead.class.getName()).log(Level.SEVERE, null, ex);
 		} catch (FileNotFoundException ex) {
@@ -88,23 +104,69 @@ public class ExcelMultiRead {
 	}
 
 	public static void echoAsCSV(Sheet sheet, OutputStreamWriter osw) throws IOException {
-		System.out.println("sheet.getSheetName()" + sheet.getSheetName());
+		//System.out.println("sheet.getSheetName()" + sheet.getSheetName());
 		Row row = null;
-		System.out.println("sheet.getLastRowNum()=" + sheet.getLastRowNum());
-		for (int i = 0; i <= sheet.getLastRowNum(); i++) {
-			row = sheet.getRow(i);
-			if (row != null) {
-				for (int j = 0; j < row.getLastCellNum(); j++) {
-					String line = row.getCell(j) + ",";
+		String header = "ZoneID, Description, Reserve(%load)";
+		osw.write(header + "\n");
+		//System.out.println("sheet.getLastRowNum()=" + sheet.getLastRowNum());
+		for (int rowIndex = 1	; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+			row = sheet.getRow(rowIndex);
 
-					osw.write(line);
-					System.out.println("line="+line);
+			if (row != null) {
+				Cell mappedLoadZoneID = row.getCell(6);
+				if (mappedLoadZoneID.getNumericCellValue() != 0.0) {
+					Cell zoneName = row.getCell(1) ;
+					if (notSeenBefore(mappedLoadZoneID, zoneName )) {
+						osw.write(zoneName + ", " + mappedLoadZoneID + ", 0.0\n");
+						seenCellNow(mappedLoadZoneID, zoneName);
+					}
 				}
 			}
-			osw.write("\n");
+		}
+		osw.write("\n");
+	}
+
+	static class ZoneNameZoneIDPair {
+		String zoneName;
+		double mappedZoneID;
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+
+			ZoneNameZoneIDPair that = (ZoneNameZoneIDPair) o;
+
+			if (Double.compare(that.mappedZoneID, mappedZoneID) != 0) return false;
+			return zoneName != null ? zoneName.equals(that.zoneName) : that.zoneName == null;
 		}
 
+		@Override
+		public int hashCode() {
+			int result;
+			long temp;
+			result = zoneName != null ? zoneName.hashCode() : 0;
+			temp = Double.doubleToLongBits(mappedZoneID);
+			result = 31 * result + (int) (temp ^ (temp >>> 32));
+			return result;
+		}
 	}
+	static Set seenZoneName = new HashSet<ZoneNameZoneIDPair>();
+
+	static boolean notSeenBefore(Cell mappedLoadZoneID, Cell zoneName) {
+		ZoneNameZoneIDPair zoneNamePair = new ZoneNameZoneIDPair();
+		zoneNamePair.zoneName = zoneName.getStringCellValue();
+		zoneNamePair.mappedZoneID = mappedLoadZoneID.getNumericCellValue();
+		return !seenZoneName.contains(zoneNamePair);
+	}
+
+	static void seenCellNow(Cell mappedLoadZoneID, Cell zoneName) {
+		ZoneNameZoneIDPair zoneNamePair = new ZoneNameZoneIDPair();
+		zoneNamePair.zoneName = zoneName.getStringCellValue();
+		zoneNamePair.mappedZoneID = mappedLoadZoneID.getNumericCellValue();
+		seenZoneName.add(zoneNamePair);
+	}
+
 
 	private static String getFileName(File file) {
 		String fileName = file.getName();
